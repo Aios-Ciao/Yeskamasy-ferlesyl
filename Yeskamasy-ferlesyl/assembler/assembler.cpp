6fdffdcd638e74c83ab58b20859afcd36010d649
Ritchan/Aios-Ciao <thor_ichino@hotmail.co.jp>
2017-09-19 21:48:00 +0900

    initial commit

---
+++ Yeskamasy-ferlesyl/assembler/assembler.cpp
@@ -0,0 +1,11 @@
+// assembler.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Yeskamasy-ferlesyl/interpreter/interpreter.cpp
@@ -0,0 +1,11 @@
+// interpreter.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
