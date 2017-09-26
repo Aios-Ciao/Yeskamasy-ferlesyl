3fa3e435ac1bb734a07ae2b336c794c0f186a02f
Aios-Ciao <thor_ichino@hotmail.co.jp>
2017-09-26 21:19:11 +0900

    fix #3 コメントと改行文字の除去 デバッグメニューのニーモニック表示へ切り替えから実行。 コメントも改行も空白1文字に置換する。

--- Yeskamasy-ferlesyl/interpreter/frmMainForm.cs
+++ Yeskamasy-ferlesyl/interpreter/frmMainForm.cs
@@ -20,15 +20,46 @@ namespace interpreter
         private void miToMnemonic_Click(object sender, EventArgs e)
         {
             string buf;
+            string[] saSrc;
 
             buf = txtSourceCode.Text;
 
             // ここで成型
+            saSrc = buf.Split('\n');
+            buf = Tokenize(saSrc);
+
 
             txtStatementList.Text = buf;
 
             tbpDebug.Select();
             tctlEditor.SelectTab(tbpDebug.Name);
         }
+
+        // コメント除去
+        string Tokenize(string [] saSrc)
+        {
+            string sResult = "";
+
+            foreach( string sLine in saSrc )
+            {
+                int posdeli = sLine.IndexOf(';');
+
+                // コメント開始文字以降を除去
+                if( posdeli < 0 )
+                {
+                    // 見つからなかった
+                    sResult += sLine + " ";
+                }
+                else
+                {
+                    // １文字の空白文字として置換する
+                    sResult += sLine.Substring(0, posdeli) + " ";
+                }
+            }
+
+            return( sResult );
+        }
     }
+
+
 }
