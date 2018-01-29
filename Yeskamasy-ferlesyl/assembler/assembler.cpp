e6a368d12f35910bee678708e3cd6e91f5b8c2af
Ritchan/Aios-Ciao <thor_ichino@hotmail.co.jp>
2018-01-29 22:53:13 +0900

    update 引数チェックしてクラス作成時のパラメータとして指定

--- Yeskamasy-ferlesyl/assembler/assembler.cpp
+++ Yeskamasy-ferlesyl/assembler/assembler.cpp
@@ -1,10 +1,13 @@
 
 #include <cstdio>
+#include <cstdlib>
 #include "ladirvirelyl.h"
 
 
 int main(int argc, char *argv[])
 {
+	if (argc == 1) exit(1);
+
 	ladirvirelyl	lv(argc, argv);
 
 	return 0;
