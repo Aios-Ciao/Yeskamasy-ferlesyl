03cb15823883b2eb2139bc41898e646a2e9e56fe
Ritchan/Aios-Ciao <thor_ichino@hotmail.co.jp>
2018-02-03 19:45:55 +0900

    add ニーモニックエンコーダー バイナリに変換するクラス追加

---
+++ Yeskamasy-ferlesyl/assembler/Encoder.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include "Encoder.h"
+
+using namespace std;
+
+bool Encoder::isMnemonic(std::string &token)
+{
+	return false;
+}
---
+++ Yeskamasy-ferlesyl/assembler/Encoder.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+// エンコーダー
+class Encoder
+{
+private:
+
+public:
+	bool isMnemonic(std::string &token);
+
+};
