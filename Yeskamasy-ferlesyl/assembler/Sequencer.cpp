93a39daf937842d9489cff02a9de116150b7e74c
Ritchan/Aios-Ciao <thor_ichino@hotmail.co.jp>
2018-05-10 20:52:34 +0900

    add ステートメントシーケンサ追加

---
+++ Yeskamasy-ferlesyl/assembler/Sequencer.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Sequencer.h"
+
+bool Sequencer::Step()
+{
+	bool bSuccessed(false);
+	lk::tModuleID	cur_idx_module(idx_module);
+	Statement::tStatementIndex	cur_idx_statement(idx_statement);
+
+	idx_statement++;
+	if(cur_idx_statement < _modules[idx_module].vStatements.size()) {
+		// 仮に逆アセ表示としておく
+		std::cout << _modules[idx_module][cur_idx_statement].ToString() << std::endl;
+		bSuccessed = true;
+	}
+
+	return(bSuccessed);
+}
+
---
+++ Yeskamasy-ferlesyl/assembler/Sequencer.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Module.h"
+
+// ステートメントシーケンサ
+class Sequencer
+{
+	Module::tModuleList	&_modules;
+	lk::tModuleID	idx_module;
+	Statement::tStatementIndex	idx_statement;
+
+public:
+	Sequencer(Module::tModuleList& modules)
+		: _modules(modules), idx_module(0), idx_statement(0)
+	{
+
+	}
+
+	~Sequencer() {};
+
+	bool Step();
+};
