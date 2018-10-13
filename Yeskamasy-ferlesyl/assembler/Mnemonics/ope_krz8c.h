b900a0335a84364fffbba28e0343b6fd13f0af50
Ritchan/Aios-Ciao <thor_ichino@hotmail.co.jp>
2018-10-13 11:48:29 +0900

    add 8bit,16bit操作命令追加

---
+++ Yeskamasy-ferlesyl/assembler/Mnemonics/ope_krz8c.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+#include <string>
+#include "../Mnemonic.h"
+
+class ope_krz8c : public Mnemonic
+{
+private:
+	enum ePrmType{
+		eSource,
+		eTarget,
+		eParamCount
+	};
+	const std::string name = "krz8c";
+
+	// 引数位置テーブル								0			1
+	const ePrmType	tbl_prmidx_ic[eParamCount] = {	eSource,	eTarget };
+	const ePrmType	tbl_prmidx_ci[eParamCount] = {	eTarget,	eSource };
+
+public:
+	bool chkApplicable(std::string &token)
+	{
+		bool bok(false);
+
+		bok |= !token.compare("krz8c");
+		bok |= !token.compare("kRz8c");
+
+		return(bok);
+	};
+
+	tParamCount getParamCount()
+	{
+		return(eParamCount);
+	}
+
+	tParamCount	getParamIndex(tParamCount idx, tParamDir d)
+	{
+		if (idx >= eParamCount) return(-1);	// 範囲外
+
+		switch (d)
+		{
+		case Mnemonic::eci_C_I:
+			return(tbl_prmidx_ci[idx]);
+			break;
+		case Mnemonic::eci_I_C:
+		default:
+			return(tbl_prmidx_ic[idx]);
+			break;
+		}
+	}
+
+	bool chkParamType(tParamCount idx, Parameter::ParamType type)
+	{
+		bool isOK(false);
+
+		switch (idx)
+		{
+		case eSource:
+			switch (type)
+			{
+			case Parameter::ParamType::eptRegister:
+			case Parameter::ParamType::eptImmidiate:
+			case Parameter::ParamType::eptReg_Ofs_Imm:
+			case Parameter::ParamType::eptReg_Ofs_Reg:
+			case Parameter::ParamType::eptLabel:
+				isOK = true;
+				break;
+			default:
+				break;
+			}
+			break;
+		case eTarget:
+			switch (type)
+			{
+			case Parameter::ParamType::eptRegister:
+			case Parameter::ParamType::eptReg_Ofs_Imm:
+			case Parameter::ParamType::eptReg_Ofs_Reg:
+				// レジスタかメモリ間接の場合だけOK
+				isOK = true;
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+
+		return(isOK);
+	}
+	std::string getName()
+	{
+		return (name);
+	}
+	// 命令の実行
+	bool Execute(Proc &proc, Parameter::tParamList &prm, tParamDir d)
+	{
+		bool bSuccess(true);
+		Ferlesexiayl::tRegister	work;
+		Ferlesexiayl::tRegister	work2;
+
+		bSuccess &= proc.Read(work, prm[eSource]);
+		bSuccess &= proc.Read(work2, prm[eTarget]);
+		work2 = (work2 & 0x00FFFFFF) | ((work & 0x000000FF) << 24);
+		bSuccess &= proc.Write(prm[eTarget], work);
+
+		return(bSuccess);
+	};
+};
+
